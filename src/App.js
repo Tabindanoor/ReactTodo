6659ea5d69a1def96d8ce611625edd4d22efd918
TabindaNoor <tabindanoor415@gmail.com>
2024-02-27 18:56:05 +0500

    bg

--- src/App.js
+++ src/App.js
@@ -1,93 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Document, HeadingLevel } from 'mammoth';
-import { generateSlides } from 'react-pptx-generator';
+import React from 'react'
 
-const WordToPptx = () => {
-  const [document, setDocument] = useState(null);
-  const [slides, setSlides] = useState([]);
-
-  useEffect(() => {
-    const loadDocument = async () => {
-      const response = await fetch('/path/to/word/document.docx');
-      const text = await response.text();
-      const file = new Document({ text });
-      const { value: html } = await file.render();
-      setDocument(html);
-    };
-
-    loadDocument();
-  }, []);
-
-  useEffect(() => {
-    if (document) {
-      const text = document.replace(/<[^>]+>/g, '');
-      const headings = text.match(/#+ [^\n]+/g);
-      const slides = [];
-
-      headings.forEach((heading, index) => {
-        const slide = {
-          elements: [
-            {
-              type: 'text',
-              text: heading.replace(/#+ (.+)/, '$1'),
-              options: {
-                x: 1,
-                y: 1.5,
-                width: 9,
-                height: 1,
-                fontSize: 48,
-                fontFamily: 'Arial',
-                color: '#000000',
-                bold: true,
-                background:'blue',
-                alignment: 'left',
-              },
-            },
-          ],
-        };
-
-        if (index % 2 === 0) {
-          slide.elements.push({
-            type: 'image',
-            image: 'https://example.com/image1.png',
-            options: {
-              x: 10,
-              y: 2,
-              width: 3,
-              height: 3,
-            },
-          });
-        } else {
-          slide.elements.push({
-            type: 'icon',
-            icon: <Icon icon="check" size={48} />,
-            options: {
-              x: 10,
-              y: 2,
-              width: 1,
-              height: 1,
-            },
-          });
-        }
-
-        slides.push(slide);
-      });
-
-      setSlides(slides);
-    }
-  }, [document]);
-
+const App = () => {
   return (
     <div>
-    {/* <br /> */}
-      {slides.length > 0 && (
-        <button onClick={() => generateSlides(slides, 'output.pptx')}>
-          Download PPTX
-        </button>
-      )}
-      {slides.length === 0 && <div>Loading...</div>}
+      <p>this is a paragraph</p>
     </div>
-  );
-};
+  )
+}
 
-export default WordToPptx;
+export default App
